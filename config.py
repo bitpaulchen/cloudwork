df0ff946055ad68cb7cd126e6a87062194b023c4
程鹏 <chengpeng@chengpengdeMacBook-Pro.local>
2022-04-24 17:00:16 +0800

    daupte

--- config.py
+++ config.py
@@ -6,15 +6,30 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/microblog"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
+    print(os.environ)
+
+    if 'RDS_DB_NAME' in os.environ:
+        SQLALCHEMY_DATABASE_URI = \
+            'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'.format(
+                username=os.environ['RDS_USERNAME'],
+                password=os.environ['RDS_PASSWORD'],
+                host=os.environ['RDS_HOSTNAME'],
+                port=os.environ['RDS_PORT'],
+                database=os.environ['RDS_DB_NAME'],
+            )
+    else:
+        print("RDS_DB_NAME")
+        SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:12345678@flaskapp.cce4qvbm0cza.us-east-1.rds.amazonaws.com/microblog"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    MAIL_SERVER = "smtp.126.com"
+    MAIL_PORT = 25
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = "chengpemgbit@126.com"
+    MAIL_PASSWORD = "BMIMZRJOKOJTWWNE"
+    ADMINS = ['chengpemgbit@126.com']
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
