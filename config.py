23b096b7f94fc81c1cc5eaa95adf5d4d33554a90
程鹏 <chengpeng@chengpengdeMacBook-Pro.local>
2022-04-24 12:07:56 +0800

    add

--- config.py
+++ config.py
@@ -11,7 +11,20 @@ class Config(object):
     #     'postgres://', 'postgresql://') or \
     #     'sqlite:///' + os.path.join(basedir, 'app.db')
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/microblog"
+    print(os.environ)
+
+    if 'RDS_DB_NAME' in os.environ:
+        SQLALCHEMY_DATABASE_URI = \
+            'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'.format(
+                username=os.environ['RDS_USERNAME'],
+                password=os.environ['RDS_PASSWORD'],
+                host=os.environ['RDS_HOSTNAME'],
+                port=os.environ['RDS_PORT'],
+                database=os.environ['RDS_DB_NAME'],
+            )
+    else:
+        print("RDS_DB_NAME")
+        SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:12345678@flaskapp.cce4qvbm0cza.us-east-1.rds.amazonaws.com/microblog"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = "smtp.126.com"
