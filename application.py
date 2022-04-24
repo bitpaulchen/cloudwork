2e2392c06f94bd64665ff6da73f15873a06e538b
程鹏 <chengpeng@chengpengdeMacBook-Pro.local>
2022-04-24 18:53:52 +0800

    update

--- application.py
+++ application.py
@@ -1,17 +1,12 @@
 from app import create_app, db, cli
 from app.models import User, Post
 
-app = create_app()
+application = create_app()
 # cli.register(app)
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
 
 
 
 
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
